b3fa2d2d215713f5c07a8076b6bd8183129f6238
0xpantera <0xpantera@proton.me>
2023-09-20 17:45:34 -0600

    add FieldElement

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,30 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::fmt::Display;
+
+#[derive(Debug)]
+struct FieldElement {
+    num: i128,
+    prime: i128,
+}
+
+impl FieldElement {
+    pub fn from(num: i128, prime: i128) -> Self {
+        if num >= prime || num < 0 {
+           panic!("Num {} not in field of order {}", num, prime);
+        }
+        Self { num, prime }
+    }
+}
+
+impl PartialEq for FieldElement {
+    fn eq(&self, other: &Self) -> bool {
+        self.num == other.num && self.prime == other.prime
+    }
+}
+
+impl Display for FieldElement {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "FieldElement_{}({})", self.prime, self.num)
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +32,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_field_elem_eq() {
+        let a = FieldElement::from(7, 13);
+        let b = FieldElement::from(6, 13);
+
+        assert_eq!(a, a);
+        assert_ne!(a, b);
     }
 }
